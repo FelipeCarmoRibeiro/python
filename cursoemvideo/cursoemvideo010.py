6b3a75fb6c04603bdef53d10b1ceddcef21fe456
Felipe do Carmo Ribeiro <felipedocarmoribeiro@gmail.com>
2025-01-25 00:42:38 -0300

    cursoemvideo

---
+++ cursoemvideo/cursoemvideo010.py
@@ -0,0 +1,4 @@
+import math
+
+num = float(input('Digite um numero: '))
+print ('O valor digitado foi {} e a sua porçao inteira é {}'.format(num, math.trunc(num) ))
---
+++ cursoemvideo/cursoemvideo07.py
@@ -0,0 +1,7 @@
+import random
+import time
+n = random.random()
+print(n)
+time.sleep(3)
+a = random.randint(1, 100)
+print(a)
