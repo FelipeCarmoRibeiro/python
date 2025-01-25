f565b5543c47b35e9fcecbdac5330255b4f2426b
Felipe do Carmo Ribeiro <felipedocarmoribeiro@gmail.com>
2025-01-25 00:51:29 -0300

    math library

--- cursoemvideo/cursoemvideo010.py
+++ cursoemvideo/cursoemvideo010.py
@@ -1,4 +1,7 @@
-import math
+'''from math import trunc
 
 num = float(input('Digite um numero: '))
-print ('O valor digitado foi {} e a sua porçao inteira é {}'.format(num, math.trunc(num) ))
+print ('O valor digitado foi {} e a sua porçao inteira é {}'.format(num, trunc(num) ))'''
+
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porçao inteira é {}'.format(num, int(num) ))
