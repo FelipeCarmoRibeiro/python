618737580ed1ff8a665d8daaeffd442fad6258ef
Felipe do Carmo Ribeiro <felipedocarmoribeiro@gmail.com>
2025-05-02 18:22:54 -0300

    att

---
+++ cursoemvideo/ex45.py
@@ -0,0 +1,12 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+
+if n1 > n2:
+    print('O primeiro valor é maior')
+elif n1 < n2:
+    print('O segundo valor é maior')
+elif n1 == n2:
+    print('Nao existe valor maior, os dois sao iguais')
+
+            #or     else:
+            #           print('Nao existe valor maior, os dois sao iguais')
